77acd15050ce73992383712a7ae1436b8c9a6280
Aniket Dandawate <anidandawate1@gmail.com>
2021-12-05 23:46:40 +0530

    Time: 132 ms (83.48%), Space: 43 MB (10.03%) - LeetHub

---
+++ two-sum/two-sum.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++) {
+            int complement = target - nums[i];
+            if (map.ContainsKey(complement)) {
+                return new int[] { map[complement], i };
+            }
+            if(map.ContainsKey(nums[i])) map[nums[i]] = i; else map[nums[i]] = i;
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+}
