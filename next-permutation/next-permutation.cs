db214d26f205e3925a53953767c1579cd2062b3b
Aniket Dandawate <anidandawate1@gmail.com>
2021-12-06 00:58:54 +0530

    Time: 116 ms (93.58%), Space: 41.5 MB (48.32%) - LeetHub

---
+++ next-permutation/next-permutation.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public void NextPermutation(int[] nums) {
+        int i = nums.Length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.Length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.Length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
