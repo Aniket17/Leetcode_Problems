7e4159924311aeef14869ab700f30b5b395b4833
Aniket Dandawate <anidandawate1@gmail.com>
2021-12-06 00:33:33 +0530

    Time: 184 ms (95.56%), Space: 47.5 MB (17.22%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    int fee = 0;
+    int[] memo;
+    public int MaxProfit(int[] prices, int fee) {
+        int cash = 0, hold = -prices[0];
+        for (int i = 1; i < prices.Length; i++) {
+            cash = Math.Max(cash, hold + prices[i] - fee);
+            hold = Math.Max(hold, cash - prices[i]);
+        }
+        return cash;
+    }
+}
