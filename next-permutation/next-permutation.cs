812f940fc606ebfa1a0145b951f831d6395467b7
Aniket Dandawate <anidandawate1@gmail.com>
2021-12-09 10:03:23 +0530

    Time: 120 ms (84.96%), Space: 41.6 MB (37.32%) - LeetHub

--- next-permutation/next-permutation.cs
+++ next-permutation/next-permutation.cs
@@ -1,31 +1,51 @@
 public class Solution {
     public void NextPermutation(int[] nums) {
-        int i = nums.Length - 2;
-        while (i >= 0 && nums[i + 1] <= nums[i]) {
-            i--;
+        var len = nums.Length;
+        var swapPos = -1;
+        for(int i = len - 1; i > 0; i--){
+            if(nums[i] > nums[i - 1]){
+                swapPos = i;
+                break;
+            }
         }
-        if (i >= 0) {
+        if(swapPos > 0){
             int j = nums.Length - 1;
-            while (nums[j] <= nums[i]) {
+            while (nums[j] <= nums[swapPos - 1]) {
                 j--;
             }
-            swap(nums, i, j);
+            Swap(nums, swapPos - 1, j);
+            SortAsc(nums, swapPos);
+            return;
         }
-        reverse(nums, i + 1);
+        Array.Reverse(nums);
+        return;
     }
-
-    private void reverse(int[] nums, int start) {
-        int i = start, j = nums.Length - 1;
-        while (i < j) {
-            swap(nums, i, j);
-            i++;
-            j--;
+    
+    
+    private void Swap(int[] nums, int pos1, int pos2){
+        var temp = nums[pos1];
+        nums[pos1] = nums[pos2];
+        nums[pos2] = temp;
+    }
+    
+    private void SortAsc(int[] nums, int pos){
+        var left = pos;
+        var right = nums.Length - 1;
+        while(left < right){
+            Swap(nums, right, left);
+            left++;
+            right--;
         }
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
+
+/*
+start from end
+if current digit > i - 1, swap, return
+
+[1,2,1,0]
+[2,0,1,1]
+
+[2,0,1,1]
+
+*/
