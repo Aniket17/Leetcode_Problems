bd9e49dabc446b720b9cba1af734746c86ff2e0a
Aniket Dandawate <anidandawate1@gmail.com>
2021-12-27 00:10:33 +0530

    Time: 84 ms (64.42%), Space: 36.4 MB (39.08%) - LeetHub

---
+++ decode-string/decode-string.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public string DecodeString(string s) {
+        if (String.IsNullOrEmpty(s)) return s;
+        
+        Stack<char> stack = new Stack<char>();
+        
+        foreach(char c in s){
+            if(c != ']'){
+                stack.Push(c);
+            }else{
+                var curr = new StringBuilder();
+                while(stack.Peek() != '['){
+                    curr.Append(stack.Pop());
+                }
+                stack.Pop();
+                var rep = 0;
+                var repCount = 0;
+                while(stack.Count > 0 && Char.IsDigit(stack.Peek())){
+                    rep += (int)(stack.Pop() - '0') * (int)Math.Pow(10, repCount++);
+                }
+                Console.WriteLine(rep);
+                var str = curr.ToString();
+                while(rep-- > 0){
+                    for(int i = str.Length - 1; i >= 0; i--){
+                        stack.Push(str[i]);
+                    }
+                }
+            }
+        }
+        var sb = new StringBuilder();
+        var cnt = stack.Count - 1;
+        while(stack.Count > 0){
+            var c = stack.Pop();
+            sb.Append(c);
+        }
+        return new String(sb.ToString().Reverse().ToArray());
+    }
+
+}
