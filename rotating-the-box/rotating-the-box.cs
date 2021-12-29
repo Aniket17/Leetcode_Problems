f16a59e01e8a7d22d370e212b6545bf039d5055f
Aniket Dandawate <anidandawate1@gmail.com>
2021-12-29 21:51:33 +0530

    Time: 1168 ms (71.28%), Space: 57.1 MB (54.26%) - LeetHub

---
+++ rotating-the-box/rotating-the-box.cs
@@ -0,0 +1,44 @@
+public class Solution {
+    public char[][] RotateTheBox(char[][] box) {
+        var m = box.Length;
+        var n = box[0].Length;
+        
+        var newBox = new char[n][];
+        for(int i = 0; i < n; i++){
+            newBox[i] = new char[m];
+        }
+        
+        // process
+        for(int i = 0; i < m; i++){
+            for(int j = n - 1; j >= 0; j--){
+                if(box[i][j] == '#'){
+                    var empty = j + 1;
+                    while(empty < n && box[i][empty] == '.'){
+                        empty++;
+                    }
+                    if(empty - 1 < n && box[i][empty - 1] == '.'){
+                        box[i][j] = '.';
+                        box[i][empty - 1] = '#';
+                    }else if(empty < n && box[i][empty] == '.'){
+                        box[i][j] = '.';
+                        box[i][empty] = '#';
+                    }
+                }
+            }
+        }
+        var k = m - 1;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                newBox[j][k] = box[i][j];
+            }
+            k--;
+        }
+        return newBox;
+    }
+    
+    private void CopyRow(char[][] box, int row, char[][] newBox, int col2){
+        for(int row1 = 0; row1 < newBox.Length; row1++){
+            newBox[row1][col2] = box[row][row1];
+        }
+    }
+}
