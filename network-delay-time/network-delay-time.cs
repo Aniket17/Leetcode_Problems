2087fd390d1235a826f63ba8205bdb7c43a7641b
Aniket Dandawate <anidandawate1@gmail.com>
2022-01-01 19:42:28 +0530

    Time: 229 ms (33.01%), Space: 44.1 MB (74.88%) - LeetHub

--- network-delay-time/network-delay-time.cs
+++ network-delay-time/network-delay-time.cs
@@ -1,33 +1,149 @@
-class Solution {
-    Dictionary<int, int> dist;
-    public int NetworkDelayTime(int[][] times, int N, int K) {
-        Dictionary<int, List<int[]>> graph = new Dictionary<int, List<int[]>>();
-        foreach (int[] edge in times) {
-            if (!graph.ContainsKey(edge[0]))
-                graph.Add(edge[0], new List<int[]>());
-            graph[edge[0]].Add(new int[]{edge[2], edge[1]});
-        }
-        foreach (int node in graph.Keys) {
-            graph[node] = graph[node].OrderBy(x=>x[0]).ToList();
-        }
-        dist = new Dictionary<int, int>();
-        for (int node = 1; node <= N; ++node)
-            dist.Add(node, int.MaxValue);
+public class Solution{
 
-        dfs(graph, K, 0);
-        int ans = 0;
-        foreach(int cand in dist.Values) {
-            if (cand == int.MaxValue) return -1;
-            ans = Math.Max(ans, cand);
-        }
-        return ans;
-    }
+        public class Edge {
+            public int key;
+            public int weight;
 
-    public void dfs(Dictionary<int, List<int[]>> graph, int node, int elapsed) {
-        if (elapsed >= dist[node]) return;
-        dist[node] = elapsed;
-        if (graph.ContainsKey(node))
-            foreach(int[] info in graph[node])
-                dfs(graph, info[1], elapsed + info[0]);
+            public Edge(int k, int w)
+            {
+                key = k;
+                weight = w;
+            }
+        }
+
+        public class MinHeap {
+            List<Edge> items;
+            int count;
+            public MinHeap(int size)
+            {
+                items = new List<Edge>();
+                count = 0;
+            }
+
+            public void Enqueue(Edge e) {
+                items.Add(e);
+                items[count++] = e;
+
+                HeapifyUp();
+            }
+
+            public Edge Dequeue() {
+                var last = items[count - 1];
+                var top = items.First();
+                items[0] = last;
+                count -= 1;
+                HeapifyDown();
+                return top;
+            }
+
+            public Edge Peek() => items.First();
+
+            public int Count { get { return count; } }
+
+
+            private void HeapifyUp() {
+                var childIndex = count - 1;
+                while (childIndex > 0) {
+
+                    var parentIndex = (childIndex - 1) / 2;
+
+                    if (items[parentIndex].weight > items[childIndex].weight)
+                    {
+                        Swap(parentIndex, childIndex);
+                        childIndex = parentIndex;
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+
+            private void HeapifyDown()
+            {
+                var currentIndex = 0;
+                var leftIndex = 1;
+                var rightIndex = 2;
+                while (leftIndex < count || rightIndex < this.count)
+                {
+                    var minChildIndex = leftIndex;
+                    if (rightIndex < this.count && this.items[rightIndex].weight < this.items[leftIndex].weight)
+                    {
+                        minChildIndex = rightIndex;
+                    }
+
+                    if (this.items[minChildIndex].weight < this.items[currentIndex].weight)
+                    {
+                        Swap(minChildIndex, currentIndex);
+
+                        currentIndex = minChildIndex;
+                        leftIndex = (currentIndex * 2) + 1;
+                        rightIndex = (currentIndex * 2) + 2;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            private void Swap(int i, int j) {
+                var tmp = items[j];
+                items[j] = items[i];
+                items[i] = tmp;
+            }
+        }
+
+
+        public int NetworkDelayTime(int[][] times, int n, int k) {
+            var ans = 0;
+
+            var heap = new MinHeap(n);
+
+            var graph = new Dictionary<int, List<Edge>>();
+
+            var map = new Dictionary<int, int>();
+
+            for (int i = 1; i < n + 1; i++)
+            {
+                graph[i] = new List<Edge>();
+                map[i] = int.MaxValue;
+            }
+
+            foreach(var time in times){
+                graph[time[0]].Add(new Edge(time[1], time[2]));
+            }
+
+            map[k] = 0;
+
+            heap.Enqueue(new Edge(k, 0));
+
+            HashSet<int> seen = new HashSet<int>();
+
+            while (heap.Count != 0) {
+                var node = heap.Dequeue();
+
+                if (seen.Contains(node.key)) continue;
+
+                map[node.key] = Math.Min(map[node.key], node.weight);
+
+                foreach (var nei in graph[node.key])
+                {
+                    var dist = nei.weight + map[node.key];
+                    if (dist < map[nei.key])
+                    {
+                        map[nei.key] = dist;
+                    }
+                    if (!seen.Contains(nei.key)) {
+                        heap.Enqueue(new Edge(nei.key, dist));
+                    }
+                }
+                seen.Add(node.key);
+            }
+
+            if(map.Any(x=>x.Value == int.MaxValue))
+            {
+                return -1;
+            }
+            return map.Max(x=>x.Value);
+        }
     }
-}
