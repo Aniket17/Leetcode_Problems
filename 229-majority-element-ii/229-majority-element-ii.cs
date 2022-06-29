ce41b3f51aa3cf01c2cadd7b18d0eed71297db12
Aniket Dandawate <anidandawate1@gmail.com>
2022-06-29 14:39:36 +0530

    Time: 192 ms (62.21%), Space: 44.7 MB (51.61%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cs
+++ 229-majority-element-ii/229-majority-element-ii.cs
@@ -1,15 +1,14 @@
 public class Solution {
     public IList<int> MajorityElement(int[] nums) {
         Dictionary<int, int> map = new Dictionary<int, int>();
-        IList<int> list = new List<int>();
         HashSet<int> set = new HashSet<int>();
         
         foreach(int num in nums){
             map[num] = map.GetValueOrDefault(num) + 1;
             if(map[num] > nums.Length/3){
-                if(set.Add(num)) list.Add(num);
+                set.Add(num);
             }
         }
-        return list;
+        return set.ToList();
     }
 }
