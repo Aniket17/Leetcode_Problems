7dbbf2cd0778e824c59230f995cdc9e8f9126e4c
Aniket Dandawate <anidandawate1@gmail.com>
2022-06-30 10:56:54 +0530

    Time: 147 ms (97.54%), Space: 44.7 MB (9.00%) - LeetHub

--- 1-two-sum/1-two-sum.cs
+++ 1-two-sum/1-two-sum.cs
@@ -1,25 +1,12 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         var map = new Dictionary<int, int>();
-        for(int i = 0; i < nums.Length; i++){
-            var val = target - nums[i];
-            if(map.ContainsKey(val)){
-                return new int[2]{i, map[val]};
+        for(int i = 0;i < nums.Length; i++){
+            if(map.ContainsKey(target - nums[i])){
+                return new int[]{i, map[target-nums[i]]};
             }
             map[nums[i]] = i;
         }
-        return new int[2]{-1,-1};
+        return new int[2];
     }
 }
-
-/*
-Input: nums = [2,7,11,15], target = 9
-
-map = {
-7: [0],
-2: [1],
--2: [2],
--6: [3]
-}
-
-*/
