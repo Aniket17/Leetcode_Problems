5ad89c7491804875c2e0328cbb0c7fbba5cad2e5
Aniket Dandawate <anidandawate1@gmail.com>
2022-07-09 12:42:01 +0530

    Time: 372 ms (26.33%), Space: 42.2 MB (75.71%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool CanJump(int[] nums) {
+        int size = nums.Length;
+    if(size <=1) return true; 
+    int badIndex = 0;
+    for(int i = size - 2; i >=0; i--){
+          if(nums[i] <= badIndex){
+                badIndex++;
+          }else{
+                badIndex = 0;
+          }
+    }
+    return nums[0] > badIndex;
+    }
+}
