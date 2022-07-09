329e6101fb1bea342c661ddef6d21086879436c6
Aniket Dandawate <anidandawate1@gmail.com>
2022-07-09 12:59:42 +0530

    Time: 181 ms (39.90%), Space: 39.3 MB (85.94%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cs
+++ 45-jump-game-ii/45-jump-game-ii.cs
@@ -1,30 +1,22 @@
 public class Solution {
     public int Jump(int[] nums) {
-        var n = nums.Length;
-        int[] minJumps = new int[n];
-        minJumps[n - 1] = 0;
-        for(int i = n - 2; i >=0; i--){
-            if(nums[i] == 0){
-                  minJumps[i] = 0;   
-                }else if(i + nums[i] >= n - 1){
-                    minJumps[i] = 1;
-                }else{
-                    //find min from i too i+nums[i]
-                    var min = GetMinValueBetween(i+1, i + nums[i], minJumps);
-                    if(min != 0){
-                        minJumps[i] = min + 1;
-                    }
+        if (nums.Length == 1) {
+            return 0;
+        }
+        int count = 0;
+        int i = 0;
+        while (i + nums[i] < nums.Length - 1) {
+            int maxVal = 0;
+            int maxValIndex = 0;
+            for (int j = 1; j <= nums[i]; j++) {
+                if (nums[j + i] + j > maxVal) {
+                    maxVal = nums[j + i] + j;
+                    maxValIndex = i + j;
                 }
+            }
+            i = maxValIndex;
+            count++;
         }
-        return minJumps[0];
-    }
-    
-    private int GetMinValueBetween(int i, int j, int[] minJumps){
-        var min = int.MaxValue;
-        for(; i <= j; i++){
-            if(minJumps[i] > 0)
-                min = Math.Min(minJumps[i], min);
-        }
-        return min == int.MaxValue ? 0: min;
+        return count + 1;
     }
 }
