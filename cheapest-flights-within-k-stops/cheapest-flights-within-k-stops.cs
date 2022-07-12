981adb35891356269911bed761b0f8588b6d8e6d
Aniket Dandawate <anidandawate1@gmail.com>
2022-07-12 23:19:47 +0530

    Time: 123 ms, Memory: 40.2 MB - LeetHub

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cs
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cs
@@ -1,76 +1,25 @@
 public class Solution {
-    int minPrice = int.MaxValue;
-    Dictionary<string, int> memo;
     public int FindCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        if(src == dst) return 0;
+        int[] distances = new int[n];
+        int[] prev = new int[n];
+        Array.Fill(distances, int.MaxValue);
+        Array.Fill(prev, int.MaxValue);
+        distances[src] = 0;
+        prev[src] = 0;
         
-        var graph = new Dictionary<int, Dictionary<int, int>>();
-        
-        memo = new Dictionary<string, int>();
-        
-        for(int i = 0; i < n; i++){
-            graph[i] = new Dictionary<int, int>();
-        }
-        
-        foreach(var f in flights){
-            var n1 = f[0];
-            var n2 = f[1];
-            var price = f[2];
-            
-            graph[n1].Add(n2, price);
-        }
-        
-        if(k == 0){
-            //no stops
-            if(graph[src].ContainsKey(dst)){
-                return graph[src][dst];
-            }else{
-                return -1;
-            }
-        }
-        
-        //BFS
-        var minPrice = int.MaxValue;
-        var queue = new Queue<Tuple<int, int>>();
-        
-        int[] prices = new int[n];
-        Array.Fill(prices, -1);
-        
-        queue.Enqueue(Tuple.Create(src, 0));
-        
-        while(queue.Count > 0){
-            var size = queue.Count;
-            if(k < 0) break; 
-            while(size-- > 0){
-                var tuple = queue.Dequeue();
-                var node = tuple.Item1;
-                var currPrice = tuple.Item2;
-                
-                foreach(var pair in graph[node]){
-                    int price = currPrice + pair.Value;
-                    
-                    if(prices[pair.Key] == -1 || prices[pair.Key] > price){
-                        
-                        prices[pair.Key] = price;
-                        
-                        queue.Enqueue(Tuple.Create(pair.Key, price));
-                    }
+        for(int i = 0; i <= k; i++){
+            distances[src] = 0;
+            foreach(var fl in flights){
+                var u = fl[0];
+                var v = fl[1];
+                var price = fl[2];
+                if(prev[u] != int.MaxValue){
+                    distances[v] = Math.Min(distances[v], prev[u] + price);
                 }
             }
-            k--;
+            prev = distances.ToArray();
         }
-        
-        return prices[dst];
+        return distances[dst] == int.MaxValue ? -1 : distances[dst];
     }
 }
-
-/*
-
-a -> b 100
-a -> c 200
-b -> c 50 
-
-dfs -> src -> dst
-src -> changes increase currK 
-if currK > k return max
-
-*/
