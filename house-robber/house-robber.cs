33a3a685f3886e755261ded52c027846dd276d0a
Aniket Dandawate <anidandawate1@gmail.com>
2022-07-14 13:01:01 +0530

    Time: 120 ms, Memory: 36.9 MB - LeetHub

--- house-robber/house-robber.cs
+++ house-robber/house-robber.cs
@@ -3,15 +3,16 @@ public class Solution {
     public int Rob(int[] nums) {
         memo = new int[nums.Length];
         Array.Fill(memo, -1);
-        return Solve(nums, 0);
+        var startAt1 = RobHouse(nums, 0);
+        var startAt2 = RobHouse(nums, 1);
+        return Math.Max(startAt1, startAt2);
     }
     
-    public int Solve(int[] nums, int i) {
-        if(i >= nums.Length){
-            return 0;
-        }
-        if(memo[i] > -1) return memo[i];
-        memo[i] = Math.Max(Solve(nums, i + 2) + nums[i], Solve(nums, i + 1));
-        return memo[i];
+    private int RobHouse(int[] nums, int houseNumber){
+        if(houseNumber >= nums.Length) return 0;
+        if(memo[houseNumber] != -1) return memo[houseNumber];
+        var robbed = nums[houseNumber] + RobHouse(nums, houseNumber + 2);
+        var notRobbed = RobHouse(nums, houseNumber + 1);
+        return memo[houseNumber] = Math.Max(robbed, notRobbed);
     }
 }
