cd5b70c96dcc399a067f6d3793bcee2cf29cef4e
Aniket Dandawate <anidandawate1@gmail.com>
2022-07-14 11:12:10 +0530

    Time: 330 ms (63.11%), Space: 50 MB (5.80%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cs
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cs
@@ -1,26 +1,14 @@
 public class Solution {
-    
-    // find max till now array
-    // choose the max
-    // [7,1,5,3,6,4]
-    // [7,6,6,6,6,4]
-    // [7,6,4,3,1]
-    // [7,6,4,3,1]
-    
-    
     public int MaxProfit(int[] prices) {
-        var n = prices.Length;
-        var nextMaxPrice = new int[n];
-        nextMaxPrice[n-1] = prices[n-1];
-        
-        for(int i = n - 2; i >= 0; i--){
-            nextMaxPrice[i] = Math.Max(prices[i], nextMaxPrice[i + 1]);
-        }
+        var minSoFar = int.MaxValue;
         var maxProfit = 0;
-        for(int i = 0; i < n; i++){
-            maxProfit = Math.Max(maxProfit, nextMaxPrice[i] - prices[i]);
+        for(int i = 0; i < prices.Length; i++){
+            if(prices[i] < minSoFar){
+                minSoFar = prices[i];
+            }else{
+                maxProfit = Math.Max(maxProfit, prices[i] - minSoFar);
+            }
         }
-        
         return maxProfit;
     }
 }
