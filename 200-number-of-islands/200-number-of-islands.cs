00f5fddc5db4ea8bc67e58000fb0723a5838f379
Aniket Dandawate <anidandawate1@gmail.com>
2022-07-18 00:01:12 +0530

    Time: 206 ms (26.16%), Space: 44.1 MB (83.23%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cs
+++ 200-number-of-islands/200-number-of-islands.cs
@@ -1,10 +1,10 @@
 public class Solution {
-    int[][] dirs = new int[4][]{
-        new int[2]{1,0},
-        new int[2]{0,1},
-        new int[2]{0,-1},
-        new int[2]{-1,0},
-    };
+    int[][] directions = new int[][]{
+            new int[]{1,0},
+            new int[]{0,1},
+            new int[]{-1,0},
+            new int[]{0,-1},
+        };
     public int NumIslands(char[][] grid) {
         var m = grid.Length;
         var n = grid[0].Length;
@@ -12,7 +12,7 @@ public class Solution {
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == '1'){
-                    Visit(grid, i, j);
+                    MarkZeros(grid, i, j);
                     count++;
                 }
             }
@@ -20,17 +20,67 @@ public class Solution {
         return count;
     }
     
-    void Visit(char[][] grid, int i, int j){
-        var m = grid.Length;
-        var n = grid[0].Length;
-        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0'){
+    void MarkZeros(char[][]grid, int i, int j){
+        if(!IsValid(grid, i, j)){
             return;
         }
         grid[i][j] = '0';
-        foreach(var dir in dirs){
-            var row = i + dir[0];
-            var col = j + dir[1];
-            Visit(grid, row, col);
+        foreach(var dir in directions){
+            var row = dir[0] + i;
+            var col = dir[1] + j;
+            MarkZeros(grid, row, col);
+        }
+    }
+    
+    bool IsValid(char[][] grid, int row, int col){
+        var m = grid.Length;
+        var n = grid[0].Length;
+        
+        if(row >= 0 && col >= 0 && row < m && col < n && grid[row][col] == '1'){
+            return true;
+        }
+        return false;
+    }
+    
+    public class UnionFind{
+        int[] parents;
+        int[] ranks;
+        
+        public UnionFind(int size){
+            parents = new int[size];
+            ranks = new int[size];
+            Array.Fill(parents, -1);
+        }
+        
+        private int FindParent(int n){
+            if(n != parents[n]){
+                parents[n] = FindParent(parents[n]);
+            }
+            return parents[n];
+        }
+        
+        public void MakeSet(int n){
+            parents[n] = n;
+        }
+        
+        public void Union(int n1, int n2){
+            var p1 = FindParent(n1);
+            var p2 = FindParent(n2);
+            if(p1 == p2) return;
+            
+            if(ranks[p1] > ranks[p2]){
+                parents[p2] = p1;
+            }else if(ranks[p1] < ranks[p2]){
+                parents[p1] = p2;
+            }else{
+                parents[p1] = p2;
+                ranks[p1]++;
+            }
+        }
+        
+        public void Print(){
+            Console.WriteLine(string.Join(",", parents));
+            Console.WriteLine(string.Join(",", ranks));
         }
     }
 }
