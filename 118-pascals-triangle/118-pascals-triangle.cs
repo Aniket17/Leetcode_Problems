8878eb176fee9955ad10012f2892439c2c6e6fab
Aniket Dandawate <anidandawate1@gmail.com>
2022-07-19 11:04:29 +0530

    Time: 166 ms (21.08%), Space: 35 MB (66.64%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cs
+++ 118-pascals-triangle/118-pascals-triangle.cs
@@ -1,33 +1,23 @@
-/*
-numRows -> []
-1 => [1]
-2 => [1,1]
-3 => [1,2,1]
-4 => [1,3,3,1]
-
-for row, col => [row-2][col] + [row-2][col-1]
-skip 1 and last
-
-*/
-
 public class Solution {
     public IList<IList<int>> Generate(int numRows) {
-        IList<IList<int>> pascalsTriangle = new List<IList<int>>();
-        pascalsTriangle.Add(new List<int>(){1});
-        if(numRows == 1) return pascalsTriangle;
-        pascalsTriangle.Add(new List<int>(){1,1});
-        if(numRows == 2) return pascalsTriangle;
-        
-        for(int row = 3; row <= numRows; row++){
-            var newRow = new List<int>(){1};
-            
-            for(int col = 1; col < row - 1; col++){
-                newRow.Add(pascalsTriangle[row-2][col] + pascalsTriangle[row-2][col-1]);
+        var ans = new List<IList<int>>();
+        if(numRows == 0) return ans;
+        ans.Add(new List<int>{1});
+        if(numRows == 1) return ans;
+        ans.Add(new List<int>{1,1});
+        if(numRows == 2) return ans;
+        var curr = 3;
+        while(curr <= numRows){
+            var row = new List<int>(){1};
+            var rowNum = curr;
+            while(row.Count != rowNum - 1){
+                var prev = ans[curr - 2];
+                row.Add(prev[row.Count] + prev[row.Count - 1]);
             }
-            newRow.Add(1);
-            pascalsTriangle.Add(newRow);
+            row.Add(1);
+            ans.Add(row);
+            curr++;
         }
-        
-        return pascalsTriangle;
+        return ans;
     }
 }
