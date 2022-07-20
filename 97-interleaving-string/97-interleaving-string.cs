b01b4170efb9ab7c1d7417f73d3def185592a9d7
Aniket Dandawate <anidandawate1@gmail.com>
2022-07-20 07:54:16 +0530

    Time: 306 ms (5.20%), Space: 69.9 MB (5.20%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cs
+++ 97-interleaving-string/97-interleaving-string.cs
@@ -1,17 +1,16 @@
 public class Solution {
     Dictionary<string, bool> memo = new Dictionary<string, bool>();
     public bool IsInterleave(string s1, string s2, string s3) {
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        
+        var i = 0;
+        var j = 0;
+        var k = 0;
         var key = $"{s1},{s2},{s3}";
         if(memo.ContainsKey(key)) return memo[key];
         while(i < s1.Length && j < s2.Length && k < s3.Length){
             var ch = s3[k++];
             if(s1[i] == ch && s2[j] == ch){
-                return memo[key] = IsInterleave(s1.Substring(i + 1), s2.Substring(j), s3.Substring(k)) 
-                    || IsInterleave(s1.Substring(i), s2.Substring(j + 1), s3.Substring(k));
+                return memo[key] = IsInterleave(s1.Substring(i+1), s2.Substring(j), s3.Substring(k))
+                    || IsInterleave(s1.Substring(i), s2.Substring(j+1), s3.Substring(k));
             }else if(s1[i] == ch){
                 i++;
             }else if(s2[j] == ch){
@@ -24,9 +23,17 @@ public class Solution {
         if(i < s1.Length && k < s3.Length){
             return memo[key] = s1.Substring(i) == s3.Substring(k);
         }
+        
         if(j < s2.Length && k < s3.Length){
             return memo[key] = s2.Substring(j) == s3.Substring(k);
         }
-        return memo[key] = i == s1.Length && j == s2.Length && k == s3.Length;
+        return memo[key] = (i == s1.Length && j == s2.Length && k == s3.Length);
     }
 }
+
+/*
+a a b c c
+a a b c d
+a a b c d 
+
+*/
