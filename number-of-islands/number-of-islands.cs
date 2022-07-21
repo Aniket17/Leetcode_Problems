1349bab7915754657762c10c32a7936780cd859b
Aniket Dandawate <anidandawate1@gmail.com>
2022-07-21 19:54:56 +0530

    Time: 181 ms, Memory: 44.4 MB - LeetHub

--- number-of-islands/number-of-islands.cs
+++ number-of-islands/number-of-islands.cs
@@ -1,32 +1,90 @@
 public class Solution {
+    int[][] directions = new int[][]{
+            new int[]{1,0},
+            new int[]{0,1},
+            new int[]{-1,0},
+            new int[]{0,-1},
+        };
     public int NumIslands(char[][] grid) {
         var m = grid.Length;
         var n = grid[0].Length;
-        var count  = 0;
-        for(int row = 0; row < m; row++){
-            for(int col = 0; col < n; col++){
-                if(grid[row][col] == '1'){
-                    MarkZeros(grid, row, col);
-                    count++;
+        var uf = new UnionFind(m*n);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    uf.MakeSet(i*n+j);
                 }
-           }
+            }
         }
-        return count;
-    }
-    private void MarkZeros(char[][] grid, int i, int j){
-        if(!IsValid(grid, i, j)) return;
-        grid[i][j] = '0';
-        MarkZeros(grid, i, j + 1);
-        MarkZeros(grid, i, j - 1);
-        MarkZeros(grid, i + 1, j);
-        MarkZeros(grid, i - 1, j);
-    }
-
-    private bool IsValid(char[][] grid, int i, int j){
-        if(i < 0 || j < 0 || i > grid.Length - 1 || j > grid[0].Length - 1 || grid[i][j] == '0'){
-            return false;
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    foreach(var dir in directions){
+                        var row = dir[0] + i;
+                        var col = dir[1] + j;
+                        if(IsValid(grid, row, col)){
+                            uf.Union(i*n+j, row*n+col);
+                        }
+                    }
+                }
+            }
         }
-        return true;
+        return uf.Count;
     }
-
+    
+    bool IsValid(char[][] grid, int row, int col){
+        var m = grid.Length;
+        var n = grid[0].Length;
+        
+        if(row >= 0 && col >= 0 && row < m && col < n && grid[row][col] == '1'){
+            return true;
+        }
+        return false;
+    }
+    
+    public class UnionFind{
+        int[] parents;
+        int[] ranks;
+        int count;
+        public UnionFind(int size){
+            parents = new int[size];
+            ranks = new int[size];
+        }
+        
+        private int FindParent(int n){
+            if(n != parents[n]){
+                parents[n] = FindParent(parents[n]);
+            }
+            return parents[n];
+        }
+        
+        public void MakeSet(int n){
+            parents[n] = n;
+            count++;
+        }
+        
+        public int Count {get{return count;}}
+        
+        public void Union(int n1, int n2){
+            var p1 = FindParent(n1);
+            var p2 = FindParent(n2);
+            if(p1 == p2) return;
+            
+            if(ranks[p1] > ranks[p2]){
+                parents[p2] = p1;
+            }else if(ranks[p1] < ranks[p2]){
+                parents[p1] = p2;
+            }else{
+                parents[p1] = p2;
+                ranks[p1]++;
+            }
+            count--;
+        }
+        
+        public void Print(){
+            Console.WriteLine(string.Join(",", parents));
+            Console.WriteLine(string.Join(",", ranks));
+        }
+    }
 }
