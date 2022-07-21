28935525474b550e74c800435073a828d7bea17b
Aniket Dandawate <anidandawate1@gmail.com>
2022-07-21 23:36:22 +0530

    Time: 156 ms (37.15%), Space: 41.3 MB (43.30%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cs
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cs
@@ -8,21 +8,15 @@
  * }
  */
 public class Solution {
-
     public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-       if(root == null || root == p || root == q){
-           return root;
-       }
-
-        var left = LowestCommonAncestor(root.left, p, q);
-        var right = LowestCommonAncestor(root.right, p, q);
-        
-        if(left == null){
-            return right;
-        }else if(right == null){
-            return left;
-        }else{
+        if(root == null) return null;
+        if(root == p || root == q){
             return root;
         }
+        var left = LowestCommonAncestor(root.left, p, q);
+        var right = LowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null) return root;
+        if(left == null && right == null) return null;
+        return left != null ? left : right;
     }
 }
