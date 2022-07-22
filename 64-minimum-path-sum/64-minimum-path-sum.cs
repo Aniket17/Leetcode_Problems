234e01a18acffb04da1b51103a52596eb7b8f9a3
Aniket Dandawate <anidandawate1@gmail.com>
2022-07-22 23:03:39 +0530

    Time: 141 ms (41.11%), Space: 39 MB (32.89%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cs
+++ 64-minimum-path-sum/64-minimum-path-sum.cs
@@ -1,28 +1,18 @@
 public class Solution {
-    Dictionary<string, int> memo = new Dictionary<string, int>();
+    int min = int.MaxValue;
     public int MinPathSum(int[][] grid) {
-        // go left , down
-        // base case = i == m - 1 && j == n - 1
-        var ans = GetPathSum(grid, 0, 0);
-        return ans;
-    }
-
-    private int GetPathSum(int[][] grid, int i, int j){
         var m = grid.Length;
         var n = grid[0].Length;
-        var key = $"{i},{j}";
-        if(memo.ContainsKey(key)) return memo[key];
-        if(i == m - 1 && j == n - 1){
-            return grid[i][j];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if(i == m - 1 && j != n - 1)
+                    grid[i][j] = grid[i][j] +  grid[i][j + 1];
+                else if(j == n - 1 && i != m - 1)
+                    grid[i][j] = grid[i][j] + grid[i + 1][j];
+                else if(j != n - 1 && i != m - 1)
+                    grid[i][j] = grid[i][j] + Math.Min(grid[i + 1][j],grid[i][j + 1]);
+            }
         }
-        if(i >= m || j >= n) return int.MaxValue;
-        
-        var rightSum = GetPathSum(grid, i, j + 1);
-        var downSum = GetPathSum(grid, i + 1, j);
-        var ans = Math.Min(rightSum, downSum);
-        if(ans == int.MaxValue){
-            return ans;
-        }
-        return memo[key] = grid[i][j] + Math.Min(rightSum, downSum);
+        return grid[0][0];
     }
 }
