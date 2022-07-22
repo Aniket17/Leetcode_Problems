a2170f8b107b5cfe4299b60ba49df3f8565f3cf5
Aniket Dandawate <anidandawate1@gmail.com>
2022-07-22 23:27:52 +0530

    Time: 142 ms (64.55%), Space: 39.5 MB (72.83%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public int CoinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+		for(int i = 1; i< dp.Length; i++) dp[i] = -1;
+		
+		int ans = minCoins(amount, coins, dp);
+        if(ans == int.MaxValue) return -1;
+        return ans;
+    }
+    
+    public int minCoins(int amount, int[] coins, int[] dp) {
+		
+		if(amount == 0) return 0;
+		
+		int ans = int.MaxValue;
+		
+		for(int i = 0; i < coins.Length; i++) {
+			if(amount - coins[i] >= 0) {
+				int subAns = 0;
+				if(dp[amount - coins[i]] != -1) {
+					subAns = dp[amount - coins[i]];
+				} else {
+					subAns = minCoins(amount - coins[i], coins, dp);
+				}
+				if(subAns != int.MaxValue && 
+						subAns + 1 < ans) {
+					ans = subAns + 1;
+				}
+			}
+		}
+		return dp[amount] = ans;
+	}
+}
