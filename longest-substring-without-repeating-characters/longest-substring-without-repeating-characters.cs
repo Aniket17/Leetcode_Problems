6edcccc92ba776a3451f2088cfde7266a0e6de23
Aniket Dandawate <anidandawate1@gmail.com>
2022-07-24 16:38:56 +0530

    Time: 123 ms, Memory: 36.8 MB - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
@@ -1,39 +1,35 @@
 public class Solution {
     public int LengthOfLongestSubstring(string s) {
-        if(s.Length == 0) return 0;
-        Dictionary<char, int> map = new Dictionary<char, int>();
-        var max = 0;
-        var current = 0;
+        if(s.Length <= 1) return s.Length;
+        Dictionary<char, int> map = new();
+        int count = 0;
+        int max = 0;
         for(int i = 0, j = 0; j < s.Length; j++){
-            if(map.ContainsKey(s[j]) && map[s[j]] >= i){
-                // found repeat
-                // move i to map[s[j]]
-                // calculate the max
-                // update map[s[j]]
-                current = j - i;
-                i = map[s[j]] + 1;
-                if(i == j) current = 1;
+            if(!map.ContainsKey(s[j])){
+                map[s[j]] = j;
+                count = j - i + 1;
             }else{
-                current = j - i + 1;
+                var ind = map[s[j]];
+                if(ind >= i){
+                    i = ind+1;
+                }
+                map[s[j]] = j;
+                count = j - i + 1;
             }
-            map[s[j]] = j;
-            max = Math.Max(max, current);
+            max = Math.Max(count, max);
         }
-        
         return max;
     }
 }
-
 /*
-"ckilbkd"
-"0123456"
-
-i   j   map                     max
-0   4   c:0 k:1 i:2 l:3 b:4
-
-1   5   c:0 k:5 i:2 l:3 b:4      5
-
-1   6   c:0 k:5 i:2 l:3 b:4 d:6     
-
+pwwkew
+call i j count max
+0    0 0 1
+1    0 1 2
+2.   0 2 0
+3.   2 2 1
+4    2 3.2
+5.   2 4 3
+6.   2 5 3
+7.   
 */
-    
