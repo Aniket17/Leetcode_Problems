0972a47f3e588bcd42b24ebda63c66b7b49e35cb
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-03 22:36:49 +0530

    Time: 123 ms, Memory: 38.1 MB - LeetHub

--- reverse-linked-list/reverse-linked-list.cs
+++ reverse-linked-list/reverse-linked-list.cs
@@ -11,50 +11,25 @@
  */
 public class Solution {
     public ListNode ReverseList(ListNode head) {
-        if(head == null || head.next == null) return head;
-        var curr = head;
-        ListNode next, prev = null;
-        while(curr != null){
-            next = curr.next; // 2 -> 3 -> 4 -> 5
-            curr.next = prev; // 1 -> null
-            prev = curr;      // 1 -> null
-            curr = next;      // 2 -> 3 -> 4 -> 5  
+        if (head == null || head.next == null) {
+            return head;
         }
-        return prev;
+        ListNode p = ReverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
     }
 }
 
-
 /*
-1 -> 2 -> 3 -> 4 -> 5
+[1,2,3,4,5]
 
-next = curr.next // 2 -> 3 -> 4 -> 5
-curr.next = prev // 1 -> null
-prev = curr      // 1 -> null
-curr = next      // 2 -> 3 -> 4 -> 5  
-
--------------------
-next = curr.next // 3 -> 4 -> 5
-curr.next = prev // 2 -> 1 -> null
-prev = curr      // 2 -> 1 -> null
-curr = next      // 3 -> 4 -> 5  
-
--------------------
-next = curr.next // 4 -> 5
-curr.next = prev // 3 -> 2 -> 1 -> null
-prev = curr      // 3 -> 2 -> 1 -> null
-curr = next      // 4 -> 5  
-
--------------------
-next = curr.next // 5 -> null
-curr.next = prev // 4 -> 3 -> 2 -> 1 -> null
-prev = curr      // 4 -> 3 -> 2 -> 1 -> null
-curr = next      // 5 -> null  
-
--------------------
-next = curr.next // null
-curr.next = prev // 5 -> 4 -> 3 -> 2 -> 1 -> null
-prev = curr      // 5 -> 4 -> 3 -> 2 -> 1 -> null
-curr = next      // null 
+f s ret
+1 2
+2 3 
+3 4 
+4 5 
+5 n 5->n
+n ? n    
 
 */
