12f2bc2c1ad6a195e89745e59e99965f96c4bbec
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-06 18:01:26 +0530

    Time: 210 ms, Memory: 39.9 MB - LeetHub

--- trapping-rain-water/trapping-rain-water.cs
+++ trapping-rain-water/trapping-rain-water.cs
@@ -1,22 +1,21 @@
 public class Solution {
-    public int Trap(int[] height) {
-        var n = height.Length;
+    public int Trap(int[] heights) {
+        var n = heights.Length;
         var left = new int[n];
         var right = new int[n];
-        left[0] = height[0];
-        for(int i = 1; i <  n; i++){
-            left[i] = Math.Max(height[i], left[i-1]);
+        left[0] = heights[0];
+        for(int i = 1; i < n; i++){
+            left[i] = Math.Max(left[i - 1], heights[i]);
         }
-        right[n - 1] = height[n - 1];
+        right[n - 1] = heights[n - 1];
         for(int i = n - 2; i >= 0; i--){
-            right[i] = Math.Max(height[i], right[i+1]);
+            right[i] = Math.Max(right[i + 1], heights[i]);
         }
-        
-        var max = 0;
-        for(int i = 1; i < n - 1; i++){
-            max += Math.Min(right[i], left[i]) - height[i];
+        var area = 0;
+        for(int i = 0; i < n; i++){
+            area += Math.Min(left[i], right[i]) - heights[i];
         }
-        return max;
+        return area;
     }
 }
 
@@ -25,7 +24,5 @@ b - [0,1,0,2,1,0,1,3,2,1,2,1]
 
 r - [0,1,1,2,2,2,2,3,3,3,3,3]
 l - [3,3,3,3,3,3,3,3,2,2,2,1]
-
 a - [0,0,1,0,1,2,1,0,0,1,0,0]
-
 */
