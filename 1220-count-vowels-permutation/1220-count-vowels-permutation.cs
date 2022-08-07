8811e09945baa647fe60ce318a820668f32e7212
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-07 23:01:57 +0530

    Time: 47 ms (46.34%), Space: 25.3 MB (85.37%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    int count = 0;
+    char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u'};
+    public int CountVowelPermutation(int n) {
+        long aCount = 1, eCount = 1, iCount = 1, oCount = 1, uCount = 1;
+        int MOD = 1000000007;
+
+        for (int i = 1; i < n; i++) {
+            long aCountNew = (eCount + iCount + uCount) % MOD;
+            long eCountNew = (aCount + iCount) % MOD;
+            long iCountNew = (eCount + oCount) % MOD;
+            long oCountNew = (iCount) % MOD;
+            long uCountNew = (iCount + oCount) % MOD;
+            aCount = aCountNew;
+            eCount = eCountNew;
+            iCount = iCountNew;
+            oCount = oCountNew;
+            uCount = uCountNew;
+        }
+        long result = (aCount + eCount + iCount + oCount + uCount)  % MOD;
+        return (int)result;
+    }
+    
+}
