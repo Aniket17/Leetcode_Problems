7daa622c5257c180bc5056959cbe8996f5481450
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-08 09:02:56 +0530

    Time: 256 ms, Memory: 44.2 MB - LeetHub

--- generate-parentheses/generate-parentheses.cs
+++ generate-parentheses/generate-parentheses.cs
@@ -1,27 +1,21 @@
 public class Solution {
     public IList<string> GenerateParenthesis(int n) {
-        var res = new List<string>();
-        if(n<=0) return res;
-        Permute(res, n, 0, 0, "");
-        return res;
+        //count of open should always be greater
+        var ans = new List<string>();
+        Generate(n, 0, 0, "", ans);
+        return ans;
     }
     
-    public void Permute(List<string> result, int max, int opening, int closing, string current)     {
-        if(current.Length == max * 2){
-            result.Add(current);
+    private void Generate(int n, int open, int closed, string str, List<string> ans){
+        if(str.Length == n*2){
+            ans.Add(str);
             return;
         }
-        if(opening < max){
-            Permute(result, max, opening + 1, closing, current+"(");
+        if(open < n){
+            Generate(n, open + 1, closed, str+"(", ans);
         }
-        
-        if(closing < max && closing < opening){
-            Permute(result, max, opening, closing + 1, current+")");
+        if(closed < n && closed < open){
+            Generate(n, open, closed + 1, str+")", ans);
         }
     }
-    
 }
-
-// valid paranthesis
-// should always have more or equa number of '(' than ')'
-// should start with '(' and end with ')'
