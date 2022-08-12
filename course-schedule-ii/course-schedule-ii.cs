24d079595ba53d0cc7bc2fc6256b27033f6f69e1
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-13 00:50:42 +0530

    Time: 192 ms, Memory: 46.4 MB - LeetHub

--- course-schedule-ii/course-schedule-ii.cs
+++ course-schedule-ii/course-schedule-ii.cs
@@ -1,31 +1,70 @@
 public class Solution {
+    /**
+    take a course, check deps
+    if no deps, put it on stack, mark as done - set
+    if course is in reading, cycle - return []
+    return stack.reverse + items which are not on stack
+    */
     public int[] FindOrder(int numCourses, int[][] prerequisites) {
-      var finished = new HashSet < int > ();
-      var visited = new HashSet < int > ();
-      var graph = new Dictionary<int,HashSet<int>>();
-      while(--numCourses >= 0){
-          graph.Add(numCourses, new HashSet<int>());
-      }
-      foreach(var pos in prerequisites) {
-        graph[pos[0]].Add(pos[1]);
-      }
-
-      foreach(var key in graph.Keys) {
-        var canFinish = DFS(graph, key, visited, finished);
-        if (!canFinish) return new List<int>().ToArray();
-            finished.Add(key);
+        var toRead = Enumerable.Range(0,numCourses).ToArray();
+        if(prerequisites.Length == 0){
+            return toRead;
         }
-        return finished.ToArray();
+        var reading = new HashSet<int>();
+        var finished = new HashSet<int>();
+        var stack = new Stack<int>();
+        
+        var graph = new Dictionary<int, List<int>>();
+        foreach(var req in prerequisites){
+            if(!graph.ContainsKey(req[0])){
+                graph[req[0]] = new List<int>();
+            }
+            graph[req[0]].Add(req[1]);
+        }
+        
+        foreach(var book in toRead){
+            if(!graph.ContainsKey(book)){
+                if(finished.Add(book)){
+                    stack.Push(book);
+                }
+            }else{
+                reading.Add(book);
+                if(!ReadBook(graph, book, stack, reading, finished)){
+                    return new int[0];
+                }
+                reading.Remove(book);
+                finished.Add(book);
+            }
+        }
+        var result = new int[stack.Count];
+        while(stack.Count != 0){
+            result[stack.Count - 1] = stack.Pop();
+        }
+        return result;
     }
-
-    public bool DFS(Dictionary<int,HashSet<int>> graph, int key, HashSet <int> visited, HashSet < int > finished) {
-          if (finished.Contains(key)) return true;
-          if (visited.Contains(key)) return false;
-          visited.Add(key);
-          foreach(var nei in graph[key]) {
-            if(!DFS(graph, nei, visited, finished)) return false;
-          }
-          finished.Add(key);
-          return true;
+    
+    bool ReadBook(Dictionary<int, List<int>> graph,
+                   int book,
+                   Stack<int> stack,
+                   HashSet<int> reading,
+                   HashSet<int> finished)
+    {
+        if(!graph.ContainsKey(book)) return true;
+        foreach(var dep in graph[book]){
+            if(reading.Contains(dep)) return false;
+            if(finished.Contains(dep)) continue;
+            reading.Add(dep);
+            if(!ReadBook(graph, dep, stack, reading, finished)){
+                return false;
+            }
+            if(finished.Add(dep)){
+                stack.Push(dep);
+            }
+            reading.Remove(dep);
+        }
+        if(finished.Add(book)){
+            stack.Push(book);
+        }
+        return true;
     }
 }
