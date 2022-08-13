39e6816d2722874d8355dc51ef0552c44e47f0b7
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-13 14:56:32 +0530

    Time: 180 ms, Memory: 39.7 MB - LeetHub

--- most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cs
+++ most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cs
@@ -11,18 +11,14 @@ public class Solution {
     
     class UnionFind{
         Dictionary<int, int> parents;
-        Dictionary<int, int> ranks;
         public int Count;
         public UnionFind(int[][]stones){
             parents = new();
-            ranks = new();
             foreach(var stone in stones){
                 var row = -(stone[0] + 1);
                 var col = stone[1] + 1;
                 parents[row] = row;
                 parents[col] = col;
-                ranks[row] = 0;
-                ranks[col] = 0;
             }
             Count = parents.Count;
         }
@@ -41,15 +37,7 @@ public class Solution {
                 return;
             }
             Count--;
-            if(ranks[p1] > ranks[p2]){
-                parents[p2] = p1;
-            }else if(ranks[p1] < ranks[p2]){
-                parents[p1] = p2;
-            }else{
-                parents[p1] = p2;
-                ranks[p2]++;
-            }
-            Console.WriteLine(Count);
+            parents[p2] = p1;
         }
     }
 }
