5abaef8aae0e391b6b6fb605138099678139b40c
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-15 14:45:16 +0530

    Time: 325 ms, Memory: 43.6 MB - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.cs
+++ longest-palindromic-substring/longest-palindromic-substring.cs
@@ -1,12 +1,9 @@
 public class Solution {
     public string LongestPalindrome(string s) {
-        if (s == null || s == "") {
-            return s;
-        }
-        
         int len = s.Length;
-
-        String ans = "";
+        if(len == 0) return s;
+        
+        string ans = "";
         int max = 0;
 
         bool[][] dp = new bool[len][];
@@ -14,11 +11,16 @@ public class Solution {
         for (int j = 0; j < len; j++) {
             dp[j] = new bool[len];
             for (int i = 0; i <= j; i++) {
+                bool isSame = s[i] == s[j];
                 
-                bool judge = s[i] == s[j];
-                
-                dp[i][j] = j - i > 2 ? dp[i + 1][j - 1] && judge : judge;
-
+                if(i == j){
+                    //single char is always palindrome
+                    dp[i][j] = true;
+                } else if(j - i == 1){
+                    dp[i][j] = isSame;
+                }else{
+                    dp[i][j] = dp[i + 1][j - 1] && isSame;
+                }
                 if (dp[i][j] && j - i + 1 > max) {
                     max = j - i + 1;
                     ans = s.Substring(i, (j - i) + 1);
@@ -27,10 +29,4 @@ public class Solution {
         }
         return ans;
     }
-    
 }
-
-/*
-"aacabdkacaa"
-"aacakdbaacaa"
-*/
