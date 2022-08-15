d743baa54cf45e6b2e8f489ec6c64046d1b9ad81
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-15 14:24:40 +0530

    Time: 364 ms, Memory: 49.3 MB - LeetHub

--- peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cs
+++ peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cs
@@ -1,19 +1,30 @@
 public class Solution {
     public int PeakIndexInMountainArray(int[] arr) {
-        var left = 1;
-        var right = arr.Length - 2;
-        
-        while(left <= right){
-            var mid = left + (right - left)/2;
+        return FindPickIndex(arr, 1, arr.Length-2);
+    }
+
+    int FindPickIndex(int[] arr, int low, int high){
+        while(low <= high){
+            var mid = low + (high - low)/2;
             if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]){
                 return mid;
             }else if(arr[mid] > arr[mid - 1]){
-                left = mid + 1;
+                low = mid + 1;
             }else{
-                right = mid - 1;
+                high = mid - 1;
             }
         }
             
-        return left;
+        return low;
     }
 }
+
+/*
+ 0,1,2,3,4,5,6
+[0,1,2,3,7,6,5]
+low high mid
+0   6    3
+3   6    4
+4   6    5
+4   5    4
+*/
