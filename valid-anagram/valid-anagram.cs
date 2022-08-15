d0c9b0d9a093b8aefeb3cd40e2584dd18c5a46e4
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-15 12:33:19 +0530

    Time: 145 ms, Memory: 38.3 MB - LeetHub

--- valid-anagram/valid-anagram.cs
+++ valid-anagram/valid-anagram.cs
@@ -1,10 +1,9 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
         if(s.Length != t.Length) return false;
-        var map = new Dictionary<char, int>();
-        foreach(var ch in s) map[ch] = 1 + map.GetValueOrDefault(ch);
+        var map = new int[256];
+        foreach(var ch in s) map[ch] = 1 + map[ch];
         foreach(var ch in t){
-            if(!map.ContainsKey(ch)) return false;
             map[ch]--;
             if(map[ch] < 0) return false;
         }
