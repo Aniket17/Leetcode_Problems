912db108cc92666c0ba76cfae402f17544b72944
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-15 20:17:18 +0530

    Time: 138 ms (96.85%), Space: 42.9 MB (5.31%) - LeetHub

--- 78-subsets/78-subsets.cs
+++ 78-subsets/78-subsets.cs
@@ -1,24 +1,22 @@
 public class Solution {
     int n, k;
-    int[] nums;
-    List<IList<int>> ans = new();
     public IList<IList<int>> Subsets(int[] nums) {
-        this.nums = nums;
+        var ans = new List<IList<int>>();
         n = nums.Length;
         for(k = 0; k < n + 1; ++k){
-            Generate(0, new List<int>());
+            Generate(nums, 0, new List<int>(), ans);
         }
         return ans;
     }
     
-    private void Generate(int pos, List<int> res){
+    private void Generate(int[] nums, int pos, List<int> res, List<IList<int>> ans){
         if(res.Count == k){
             ans.Add(res.ToList());
             return;
         }
         for(int i = pos; i < n; ++i){
             res.Add(nums[i]);
-            Generate(i + 1, res);
+            Generate(nums, i + 1, res, ans);
             res.RemoveAt(res.Count - 1);
         }
     }
