e1889cf93e9d216305159516f3b6cd94049272a9
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-15 14:45:50 +0530

    Time: 323 ms, Memory: 49.1 MB - LeetHub

--- maximum-subarray/maximum-subarray.cs
+++ maximum-subarray/maximum-subarray.cs
@@ -1,28 +1,11 @@
 public class Solution {
     public int MaxSubArray(int[] nums) {
-        if(nums.Length == 0) return 0;
-        
-        int currentMax = nums[0];
-        var maxSum = currentMax;
-        for(int i = 1; i < nums.Length; i++){
-            currentMax = Math.Max(nums[i], currentMax + nums[i]);
-            maxSum = Math.Max(maxSum, currentMax);
+        var max = int.MinValue;
+        var sum = 0;
+        for(int i = 0; i < nums.Length; i++){
+            sum = Math.Max(nums[i], sum + nums[i]);
+            max = Math.Max(sum, max);
         }
-        return maxSum;
+        return max;
     }
 }
-
-/*
-[-2,1,-3,4,-1,2,1,-5,4]
-
-i   currentMax
-0   -2
-1   (-1, -2) => -1
-2   (-4, -3) => -3
-3   (4, 1) => 4
-5   (-1, 3) => 3
-6   (2, 5) => 5
-7   (1, 6) => 6
-8   (-5, 1)=> 1
-9   (4, 5) => 5
-*/
