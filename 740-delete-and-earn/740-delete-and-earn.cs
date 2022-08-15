53e13226aee138663b41ccf4cf01a7965f1ffdc4
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-15 23:53:59 +0530

    Time: 125 ms (70.60%), Space: 38.9 MB (92.58%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cs
+++ 740-delete-and-earn/740-delete-and-earn.cs
@@ -1,27 +1,30 @@
 public class Solution {
     int[] memo;
+    int[] counts;
     public int DeleteAndEarn(int[] nums) {
-        Dictionary<int, int> counts = new();
-        var memoSize = 0;
-        foreach(var n in nums){
-            memoSize = Math.Max(memoSize, n);
-            counts[n] = n + counts.GetValueOrDefault(n);
-        }
+        var memoSize = nums.Max(x=>x);
+        counts = new int[memoSize + 1];
+        
         memo = new int[memoSize + 1];
         Array.Fill(memo, -1);
         
-        return MaxPoints(counts, memoSize);
+        foreach(var n in nums){
+            counts[n] += n;
+        }
+        
+        
+        return MaxPoints(memoSize);
     }
     
-    private int MaxPoints(Dictionary<int, int> counts, int key){
+    private int MaxPoints(int key){
         if(memo[key] != -1) return memo[key];
         if(key == 0) return 0;
         
-        var profit = counts.GetValueOrDefault(key);
+        var profit = counts[key];
         if(key == 1) return profit;
         
-        var take = profit + MaxPoints(counts, key - 2);
-        var dontTake = MaxPoints(counts, key - 1);
+        var take = profit + MaxPoints(key - 2);
+        var dontTake = MaxPoints(key - 1);
         
         return memo[key] = Math.Max(dontTake, take);
     }
