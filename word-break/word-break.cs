26417c80955ff555ed3b015d7163b17deb3dbe3a
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-17 22:30:05 +0530

    Time: 156 ms, Memory: 40 MB - LeetHub

--- word-break/word-break.cs
+++ word-break/word-break.cs
@@ -1,29 +1,19 @@
 public class Solution {
-    
     Dictionary<string, bool> memo = new Dictionary<string, bool>();
-    
-    public bool WordBreak(string s, IList<string> wordDict) {
-        if(s.Length == 0) return true;
-        
-        foreach(var w in wordDict){
-            var index = s.IndexOf(w);
-            
-            if(index < 0) continue;
-            
-            var firstPart = s.Substring(0, index);
-            var secondPart = s.Substring(index + w.Length);
-            
-            if(!memo.ContainsKey(firstPart)){
-                memo[firstPart] = WordBreak(firstPart, wordDict);
+    public bool WordBreak(string s, IList<string> words) {
+        if(s == "") return true;
+        foreach(var w in words){
+            int wIndex = s.IndexOf(w);
+            if(wIndex < 0) continue;
+            var part1 = s.Substring(0, wIndex);
+            if(!memo.ContainsKey(part1)){
+                memo[part1] = WordBreak(part1, words);
             }
-            
-            if(!memo.ContainsKey(secondPart)){
-                memo[secondPart] = WordBreak(secondPart, wordDict);
+            var part2 = s.Substring(wIndex+w.Length);
+            if(!memo.ContainsKey(part2)){
+                memo[part2] = WordBreak(part2, words);
             }
-            
-            if(memo[firstPart] && memo[secondPart]){
-                return true;
-            }
+            if(memo[part1] && memo[part2]) return true;
         }
         return false;
     }
