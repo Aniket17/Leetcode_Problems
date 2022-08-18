8bad2e3362848dcb18af2ca3a1f5c87fa6124a9b
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-18 23:31:16 +0530

    Time: 140 ms, Memory: 37.6 MB - LeetHub

--- min-cost-climbing-stairs/min-cost-climbing-stairs.cs
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.cs
@@ -1,7 +1,7 @@
 public class Solution {
     int[] memo;
     public int MinCostClimbingStairs(int[] cost) {
-        memo = new int[cost.Length];
+        memo = new int[2];
         Array.Fill(memo, -1);
         var startAt0 = Climb(cost, 0);
         var startAt1 = Climb(cost, 1);
@@ -10,10 +10,10 @@ public class Solution {
     
     private int Climb(int[] costs, int pos){
         if(pos >= costs.Length) return 0;
-        if(memo[pos] != -1) return memo[pos];
+        if(memo[pos%2] != -1) return memo[pos%2];
         //climb 1 or climb 2
         var climb1 = costs[pos] + Climb(costs, pos + 1);
         var climb2 = costs[pos] + Climb(costs, pos + 2);
-        return memo[pos] = Math.Min(climb1, climb2);
+        return memo[pos%2] = Math.Min(climb1, climb2);
     }
 }
