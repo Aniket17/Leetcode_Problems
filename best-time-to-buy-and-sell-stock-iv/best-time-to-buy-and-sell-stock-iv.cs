3dcf8a448927ba8bcd300073d37b8a032f7339cd
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-18 15:47:32 +0530

    Time: 137 ms, Memory: 38.7 MB - LeetHub

--- best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cs
+++ best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cs
@@ -1,26 +1,24 @@
 public class Solution {
+    int[,,] memo;
     public int MaxProfit(int k, int[] prices) {
-        var days = prices.Length;
-        if(days < 2) return 0;
-        
-        int[][] dp = new int[k + 1][];
-        for(int i = 0; i <= k; i++){
-            dp[i] = new int[days];
+        int n = prices.Length;
+        memo = new int[n+1, k+1, 2];
+
+        return Solve(prices, 0, k, 0);
+    }
+    int Solve(int[] prices, int pos, int k, int holding){
+        // is holding -> sell and move next or dont sell and move next
+        // not holding -> hold and move next or move next
+        if(memo[pos, k, holding] != 0) return memo[pos, k, holding];
+        if(k == 0 || pos >= prices.Length) return 0;
+        int res = 0;
+        var doNothing = Solve(prices, pos+1, k, holding);
+        if(holding == 1){
+            res = prices[pos] + Solve(prices, pos+1, k-1, 0);
+        }else{
+            res = -prices[pos] + Solve(prices, pos+1, k, 1);
         }
         
-        for(int i = 1; i < dp.Length; i++){
-            var maxDiff = -prices[0];
-            
-            // dp[i][j-1] => not transacting on jth day with i transactions
-            // prices[j] + maxDiff => choose to transact
-            // dp[i - 1][j] => profit on same day from last transaction
-            
-            for(int j = 1; j < dp[0].Length; j++){
-                dp[i][j] = Math.Max(dp[i][j - 1], prices[j] + maxDiff);
-                
-                maxDiff = Math.Max(maxDiff, dp[i - 1][j] - prices[j]);
-            }
-        }
-        return dp[k][days - 1];
+        return memo[pos, k, holding] = Math.Max(doNothing, res);
     }
 }
