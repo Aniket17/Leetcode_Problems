a69a1b8e5c0950ad2bc9c709dbaf599ea5a6a433
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-21 23:06:50 +0530

    Time: 117 ms (75.64%), Space: 38.1 MB (99.79%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cs
+++ 64-minimum-path-sum/64-minimum-path-sum.cs
@@ -1,18 +1,20 @@
 public class Solution {
-    int min = int.MaxValue;
     public int MinPathSum(int[][] grid) {
-        var m = grid.Length;
-        var n = grid[0].Length;
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if(i == m - 1 && j != n - 1)
+        for (int i = grid.Length - 1; i >= 0; i--) {
+            for (int j = grid[0].Length - 1; j >= 0; j--) {
+                if(i == grid.Length - 1 && j != grid[0].Length - 1)
                     grid[i][j] = grid[i][j] +  grid[i][j + 1];
-                else if(j == n - 1 && i != m - 1)
+                else if(j == grid[0].Length - 1 && i != grid.Length - 1)
                     grid[i][j] = grid[i][j] + grid[i + 1][j];
-                else if(j != n - 1 && i != m - 1)
+                else if(j != grid[0].Length - 1 && i != grid.Length - 1)
                     grid[i][j] = grid[i][j] + Math.Min(grid[i + 1][j],grid[i][j + 1]);
             }
         }
         return grid[0][0];
     }
 }
+
+/*
+[[1,2,3],
+[4,5,6]]
+*/
