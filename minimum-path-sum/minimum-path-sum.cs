99aeb68cd8b4d80db48a1a6bbc8dd949a7947786
Aniket Dandawate <anidandawate1@gmail.com>
2022-08-21 22:03:01 +0530

    Time: 138 ms, Memory: 38.7 MB - LeetHub

--- minimum-path-sum/minimum-path-sum.cs
+++ minimum-path-sum/minimum-path-sum.cs
@@ -1,27 +1,18 @@
 public class Solution {
+    int min = int.MaxValue;
     public int MinPathSum(int[][] grid) {
         var m = grid.Length;
         var n = grid[0].Length;
-        
-        var dp = new int[m][];
-        dp[0] = new int[n];
-        dp[0][0] = grid[0][0];
-        
-        for(int i = 1; i < m; i++){
-            dp[i] = new int[n];
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
-        }
-        
-        for(int i = 1; i < n; i++){
-            dp[0][i] = dp[0][i - 1] + grid[0][i];;
-        }
-        
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                dp[i][j] = grid[i][j] + Math.Min(dp[i][j - 1], dp[i - 1][j]);
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if(i == m - 1 && j != n - 1)
+                    grid[i][j] = grid[i][j] +  grid[i][j + 1];
+                else if(j == n - 1 && i != m - 1)
+                    grid[i][j] = grid[i][j] + grid[i + 1][j];
+                else if(j != n - 1 && i != m - 1)
+                    grid[i][j] = grid[i][j] + Math.Min(grid[i + 1][j],grid[i][j + 1]);
             }
         }
-        
-        return dp[m - 1][n - 1];
+        return grid[0][0];
     }
 }
