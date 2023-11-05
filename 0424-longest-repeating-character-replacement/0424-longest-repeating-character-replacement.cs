80fe7353151a2e9ff9c3ac722a01275d6e41f266
Aniket Dandawate <anidandawate1@gmail.com>
2023-11-05 11:57:43 +0530

    Time: 50 ms (98.42%), Space: 38.2 MB (39.12%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+        var map = new int[26];
+        int maxFreq = 0, left=0, right=0;
+        var result = 0;
+        while(left <= right && right < s.Length){
+            var len = right - left + 1;
+            var ch = s[right];
+            map[ch-'A']++;
+            maxFreq = Math.Max(maxFreq, map[ch-'A']);
+            if(len - maxFreq <= k){
+                //valid window
+                result = Math.Max(result, len);
+            }else{
+                map[s[left]-'A']--;
+                left++;
+            }
+            right++;
+        }
+        Console.WriteLine(string.Join(",", map));
+        return result;
+    }
+}
