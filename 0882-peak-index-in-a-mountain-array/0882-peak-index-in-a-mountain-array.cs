fff7dc34cb08a87e204d538dce6e16d5e0c5b4ae
Aniket Dandawate <anidandawate1@gmail.com>
2024-10-26 23:52:24 +0530

    Time: 0 ms (100%), Space: 66.8 MB (70.7%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cs
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cs
@@ -1,20 +1,19 @@
 public class Solution {
     public int PeakIndexInMountainArray(int[] arr) {
+        //use binary search to get to peak element
+        //check if low..mid is sorted asc or dsc
+        //if(arr[mid] < arr[mid]) // low to mid is asc so the peak will be at right
+        //if(arr[mid] > arr[mid+1]) // low to mid is desc, so the peak will be at left
         int low = 0, high = arr.Length - 1;
-
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            if (arr[mid] < arr[mid + 1]) {
-                // We are in the increasing part of the mountain, so move right
-                low = mid + 1;
-            } else {
-                // We are in the decreasing part, so the peak is at mid or to the left
-                high = mid;
+        while(low < high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] < arr[mid+1]){
+                low = mid+1; //we are asc part, so seach right
+            }else{ 
+                high = mid; //we are in dsc part, so search left
             }
         }
 
-        // When low == high, we have found the peak
         return low;
     }
 }
