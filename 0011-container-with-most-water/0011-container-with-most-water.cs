f454ec76ebb1306cde06f665892779f755fe7171
Aniket Dandawate <anidandawate1@gmail.com>
2024-10-27 00:26:00 +0530

    Time: 1 ms (99.85%), Space: 61.8 MB (82.07%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cs
+++ 0011-container-with-most-water/0011-container-with-most-water.cs
@@ -1,21 +1,20 @@
 public class Solution {
     public int MaxArea(int[] height) {
-        var left = 0;
-        var right = height.Length - 1;
-        var maxArea = 0;
-        
+        //brute force:
+        //find all combinations in n^2 time and check the areas
+        //optimal: as area will be maximized with how far apart the lines are and how tall the lines are
+        //we can use two pointer approach here and start with max far apart lines, i.e. left and right
+        //and we will continue to move the pointer which leads us to max height
+        int left = 0, right = height.Length - 1, n = height.Length, maxArea = 0;
         while(left < right){
-            var minHeight = Math.Min(height[left], height[right]);
-            maxArea = Math.Max(maxArea, minHeight * (right-left));
+            maxArea = Math.Max(maxArea, (right-left) * Math.Min(height[left], height[right]));
             if(height[left] < height[right]){
+                //move the left hoping it will give better height
                 left++;
-            }else if(height[left] > height[right]){
-                right--;
             }else{
-                left++;
+                right--;
             }
         }
-
         return maxArea;
     }
 }
