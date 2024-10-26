041f485ca03e16d78c45068e3e610a4b145519af
Aniket Dandawate <anidandawate1@gmail.com>
2024-10-27 00:12:57 +0530

    Time: 8 ms (95.25%), Space: 42.9 MB (38.26%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
@@ -1,36 +1,32 @@
 public class Solution {
     public int LengthOfLongestSubstring(string s) {
-        /*
-        //pwwkew
-        i   ch   left    right   set  max
-        0   p      0       1      [p]  1
-        1   w      0       2      [pw] 2
-        2   w      2       2      [w]  2
-        3   k      2       3      [wk]  2
-        4   e      2       4      [wke]  2
-        5   w      2       4      [wke]  3
+        //brute force
+        //find every substring in n^2 time and check which has repeats
+        //use two pointer approach
+        //use i and j to track current substring
+        //maintain a hashset to track current characters
+        //move j until we find a char which is repeated
+        //calculate len as j-i+1
+        //move i until that repeat char is found
+        //move i one step further from first occurance of repeat char
+        //remove the char from the hashset
+        //do this till j < s.Length
 
-        */
-        var set = new HashSet<char>();//[]
-        var left = 0; 
-        var right = 0;
-        var maxLen = 0;
-        while(right < s.Length){
-            var ch = s[right];
-            if(set.Contains(ch)){
-                //move left till we find it again
-                while(s[left] != ch){
-                    set.Remove(s[left++]);
+        int n = s.Length, i = 0, j = 0, maxLen = 0, currLen = 0;
+        var seen = new HashSet<char>();
+        while(j < n){
+            var ch = s[j];
+            if(seen.Contains(ch)){
+                while(i <= j && s[i] != ch){
+                    seen.Remove(s[i]);
+                    i++;
                 }
-                //left is at repeat char, remove that as well
-                set.Remove(ch);
-                left++;
-            }else{
-                //healthy substring, recalc maxLen
-                set.Add(ch); 
-                right++;
+                i++; //move one step further   
             }
-            maxLen = Math.Max(maxLen, set.Count());
+            seen.Add(ch);
+            currLen = j - i + 1;
+            maxLen = Math.Max(maxLen, currLen);
+            j++;
         }
         return maxLen;
     }
