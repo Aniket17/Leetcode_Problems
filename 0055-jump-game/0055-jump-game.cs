dcfe428d5de8af481547c5613870612015e8a1ac
Aniket Dandawate <anidandawate1@gmail.com>
2024-10-27 13:58:38 +0530

    Time: 1 ms (100%), Space: 61.8 MB (54.04%) - LeetHub

--- 0055-jump-game/0055-jump-game.cs
+++ 0055-jump-game/0055-jump-game.cs
@@ -1,5 +1,5 @@
 public class Solution {
-    public bool CanJump(int[] nums) {
+    public bool CanJump_OLD_APPROACH(int[] nums) {
         var n = nums.Length;
         var ans = new int[n];
         ans[n-1] = n-1;
@@ -12,6 +12,20 @@ public class Solution {
         }
 
         return ans[0] <= nums[0];
+    }
+
+    public bool CanJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.Max(maxReach, i + nums[i]);
+            if (maxReach >= nums.Length - 1) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
