ebe84e6a56c1b8818fd22ad17a676b84f9e82575
Aniket Dandawate <anidandawate1@gmail.com>
2024-10-27 16:32:25 +0530

    Time: 627 ms (6.98%), Space: 136.1 MB (5.13%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cs
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cs
@@ -1,34 +1,70 @@
 public class Solution {
     public string MinWindow(string s, string t) {
-        if(t.Length > s.Length) return "";
-        var countsMap = t.GroupBy(x => x).Select(x => new { Key = x.Key, Ct = x.Count() }).ToDictionary(x => x.Key, x => x.Ct);
-
-        int left = 0, right = 0;
-        var currMap = new Dictionary<char, int>();
-        var formed = 0;
-        var required = countsMap.Keys.Count;
-        var ans = new int[]{-1,0,0}; //len, left, right
-        while(right < s.Length){
-            var ch = s[right];
-            currMap[ch] = currMap.GetValueOrDefault(ch) + 1;
-            if(countsMap.ContainsKey(ch) && currMap[ch] == countsMap[ch]){
-                formed++;
-            }
-
-            while(formed == required && left <= right){
-                ch = s[left];
-                if(ans[0] == -1 || ans[0] > right-left+1){
-                    ans = new int[]{right-left+1, left, right};
-                }
-                currMap[ch]--;
-                if(countsMap.ContainsKey(ch) && currMap[ch] < countsMap[ch]){
-                    formed--;
-                }
-                left++;
-                
-            }
-            right++;
+        //create a map of s and t
+        //check if s has all the chars+count of t otherwise early return
+        //sliding window with i and j
+        //whenever currS[char] == t[char] reduce number of remaining
+        //whenever remain == 0 then we will calculate the minwindow
+        //minmize the window by moving i until the condition satisfies remain == 0
+        var mapS = new Dictionary<char, int>();
+        var mapT = new Dictionary<char, int>();
+        foreach(var ch in s){
+            mapS[ch] = mapS.GetValueOrDefault(ch) + 1;
         }
-        return ans[0] == -1 ? "" : s.Substring(ans[1], (ans[2] - ans[1]+1));
+
+        foreach(var ch in t){
+            mapT[ch] = mapT.GetValueOrDefault(ch) + 1;
+        }
+        foreach(var k in mapT.Keys){
+            if(!mapS.ContainsKey(k) || mapS[k] < mapT[k]) return "";
+        }
+
+        int remain = mapT.Keys.Count(), i = 0, j = 0, minWindow = 0;
+        var ans = s;
+        while(j < s.Length){
+            var ch = s[j];
+            if(!mapT.ContainsKey(ch)){
+                j++; 
+                continue;
+            }
+            mapT[ch]--;
+            if(mapT[ch] == 0){
+                remain--;
+            }
+            if(remain == 0){
+                //we have our window
+                if(ans.Length > j - i + 1)
+                ans = s.Substring(i, j-i+1);
+
+                //we want to minimize this further now
+                //lets move i until we get remain == 1
+                while(remain == 0){
+                    var ich = s[i];
+                    if(mapT.ContainsKey(ich)){
+                        mapT[ich]++;
+                        if(mapT[ich] > 0){
+                            remain++;
+                        }
+                        //this is the point where we know that the current j-i+1 is the minWindow
+                        var tmp = s.Substring(i, j-i+1);
+                        if(tmp.Length < ans.Length){
+                            ans = tmp;
+                        }
+                    }
+                    i++;
+                }
+            }
+            j++;
+        }
+
+        Console.WriteLine($"remain: {remain}, mapT: {string.Join(",", mapT.Select(kv=>kv.Key + ":"+kv.Value))}");
+
+        return ans;
     }
 }
+
+/*
+ADOBECODEBANC ABC
+remain = 1
+ADO BECODEBANC
+*/
