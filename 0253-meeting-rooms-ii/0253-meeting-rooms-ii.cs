600fa7c61da2e8de6865a0912c746133a4cb3647
Aniket Dandawate <anidandawate1@gmail.com>
2024-10-27 22:17:20 +0530

    Time: 8 ms (37.21%), Space: 45 MB (27.01%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cs
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cs
@@ -1,34 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 public class Solution {
     public int MinMeetingRooms(int[][] intervals) {
-        if (intervals == null || intervals.Length == 0)
-            return 0;
+        var heap = new PriorityQueue<int[], int>();
+        intervals = intervals.OrderBy(x=>x[0]).ToArray();
+        heap.Enqueue(intervals[0], intervals[0][1]);
 
-        // Sort the intervals based on the start time
-        var sortedIntervals = intervals.OrderBy(x => x[0]).ToArray();
-
-        // Initialize a min heap (priority queue) ordered by end time
-        var minHeap = new PriorityQueue<int[], int>();
-        minHeap.Enqueue(sortedIntervals[0], sortedIntervals[0][1]);
-
-        // Iterate over the remaining intervals
-        for(int i = 1; i < sortedIntervals.Length; i++){
-            var current = sortedIntervals[i];
-            var earliestEnd = minHeap.Peek()[1];
-
-            if (earliestEnd <= current[0]){
-                // The room is free, reuse it by removing the earliest ending meeting
-                minHeap.Dequeue();
+        for(int i = 1; i < intervals.Length; i++){
+            int[] curr = intervals[i];
+            int[] top = heap.Peek();
+            if(top[1] <= curr[0]){
+                heap.Dequeue();
             }
-
-            // Allocate the current meeting to a room (new or reused)
-            minHeap.Enqueue(current, current[1]);
+            heap.Enqueue(curr, curr[1]);
         }
-
-        // The size of the heap is the number of rooms required
-        return minHeap.Count;
+        return heap.Count;
     }
 }
+
+//store meetings in min heap sorted by duration
+//add 1st to heap increase the rooms
+//for every other, check if heap.peek is overlapping, if yes, add next one and increase the rooms
+//if it does not overlap, then dequeue from heap and add new one, no increase in rooms
