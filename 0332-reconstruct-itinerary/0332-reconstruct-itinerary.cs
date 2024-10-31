8221718e9be907f240ae8bdbce2b90f871127399
Aniket Dandawate <anidandawate1@gmail.com>
2024-10-31 23:06:48 +0530

    Time: 5 ms (100%), Space: 61 MB (43.44%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cs
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cs
@@ -1,41 +1,24 @@
 public class Solution {
     public IList<string> FindItinerary(IList<IList<string>> tickets) {
-        // Step 1: Build the adjacency list
-        var adj = new Dictionary<string, List<string>>();
-        
-        // Populate the adjacency list from tickets
-        foreach (var ticket in tickets) {
-            if (!adj.ContainsKey(ticket[0])) {
-                adj[ticket[0]] = new List<string>();
-            }
-            adj[ticket[0]].Add(ticket[1]);
+        var map = new Dictionary<string, List<string>>();
+        foreach(var ticket in tickets){
+            string src = ticket[0], dst = ticket[1];
+            if(!map.ContainsKey(src)) map[src] = new();
+            map[src].Add(dst);
         }
-        
-        // Step 2: Sort each list of destinations to maintain lexicographical order
-        foreach (var key in adj.Keys) {
-            adj[key].Sort();
-        }
-
-        // Step 3: Use a stack to construct the itinerary in reverse order
+        foreach(var key in map.Keys) map[key].Sort();
         var itinerary = new Stack<string>();
 
-        // Recursive DFS function with post-order traversal
-        void Dfs(string airport) {
-            // Visit each destination in sorted order
-            while (adj.ContainsKey(airport) && adj[airport].Count > 0) {
-                // Get the next lexicographically smallest destination
-                var nextDestination = adj[airport][0];
-                adj[airport].RemoveAt(0);
-                Dfs(nextDestination);
+        void Dfs(string src){
+            while(map.ContainsKey(src) && map[src].Count > 0){
+                var nextAirport = map[src][0];
+                map[src].RemoveAt(0);
+                Dfs(nextAirport);
             }
-            // After visiting all destinations from 'airport', add it to itinerary
-            itinerary.Push(airport);
+            itinerary.Push(src);
         }
 
-        // Start DFS from "JFK"
         Dfs("JFK");
-
-        // The itinerary stack contains the path in reverse, so convert it to list and return
         return itinerary.ToList();
     }
 }
