09bffc242ec2179ec01cf72364ae006d4bc86fed
Aniket Dandawate <anidandawate1@gmail.com>
2024-11-01 14:16:25 +0530

    Time: 458 ms (47.62%), Space: 66.5 MB (65.31%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cs
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cs
@@ -1,56 +1,84 @@
 public class Solution {
-    public class Edge:IComparable<Edge>{
-        public int[] src, dst;
-        public int weight;
-        public Edge(int[] s, int[] d, int w){
-            src = s; dst=d; weight = w;
-        }
-
-        public int CompareTo(Edge other){
-            return weight.CompareTo(other.weight);
-        }
-    }
     public int MinCostConnectPoints(int[][] points) {
         var edges = new List<Edge>();
-        var uf = new UnionFind();
-        for(int i = 0; i < points.Length; i++){
-            uf.MakeSet(points[i]);
-            for(int j = i+1; j < points.Length; j++){
-                var p1 = points[i];
-                var p2 = points[j];
-                var dist = Math.Abs(p1[0] - p2[0]) + Math.Abs(p1[1] - p2[1]);
-                edges.Add(new Edge(p1, p2, dist));
+        var uf = new UnionFind(points.Length);
+
+        // Create edges between every pair of points
+        for (int i = 0; i < points.Length; i++) {
+            for (int j = i + 1; j < points.Length; j++) {
+                var dist = Math.Abs(points[i][0] - points[j][0]) + Math.Abs(points[i][1] - points[j][1]);
+                edges.Add(new Edge(i, j, dist));
             }
         }
+
+        // Sort edges by weight (Manhattan distance)
         edges.Sort();
-        var minCost = 0;
-        foreach(var edge in edges){
-            if(uf.Union(edge.src, edge.dst)){
+
+        int minCost = 0;
+        int numEdges = 0;
+
+        // Process edges to form MST using Kruskal's algorithm
+        foreach (var edge in edges) {
+            if (uf.Union(edge.src, edge.dst)) {
                 minCost += edge.weight;
+                numEdges++;
+                // Stop if we have included (n - 1) edges
+                if (numEdges == points.Length - 1) break;
             }
         }
+
         return minCost;
     }
 
-    public class UnionFind{
-        Dictionary<int[], int[]> parent = new();
-        public void MakeSet(int[] p){
-            parent[p] = p;
+    public class Edge : IComparable<Edge> {
+        public int src, dst, weight;
+        public Edge(int s, int d, int w) {
+            src = s;
+            dst = d;
+            weight = w;
         }
 
-        int[] Find(int[] id){
-            if(parent[id] == id) return id;
-            parent[id] = Find(parent[id]);
-            return parent[id];
-        }
-
-        public bool Union(int[] id1, int[] id2){
-            var p1 = Find(id1);
-            var p2 = Find(id2);
-            if(p1 == p2) return false;
-            parent[p1] = p2;
-            return true;
+        public int CompareTo(Edge other) {
+            return weight.CompareTo(other.weight);
         }
     }
 
+    public class UnionFind {
+        private int[] parent;
+        private int[] rank;
+
+        public UnionFind(int size) {
+            parent = new int[size];
+            rank = new int[size];
+            for (int i = 0; i < size; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public int Find(int node) {
+            if (parent[node] != node) {
+                parent[node] = Find(parent[node]); // Path compression
+            }
+            return parent[node];
+        }
+
+        public bool Union(int node1, int node2) {
+            int root1 = Find(node1);
+            int root2 = Find(node2);
+
+            if (root1 == root2) return false; // Already in the same set
+
+            // Union by rank
+            if (rank[root1] > rank[root2]) {
+                parent[root2] = root1;
+            } else if (rank[root1] < rank[root2]) {
+                parent[root1] = root2;
+            } else {
+                parent[root1] = root2;
+                rank[root2]++;
+            }
+
+            return true;
+        }
+    }
 }
