f69c6ad8a99acc54a5b521a22a9238027dda604f
Aniket Dandawate <anidandawate1@gmail.com>
2024-11-01 15:05:53 +0530

    Time: 242 ms (54.1%), Space: 64.2 MB (63.36%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cs
+++ 0744-network-delay-time/0744-network-delay-time.cs
@@ -1,60 +1,44 @@
-public class Solution{
-        public class Node : IComparable<Node>{
-            public int key;
-            public int weight;
-
-            public Node(int k, int w)
-            {
-                key = k;
-                weight = w;
-            }
-
-            public int CompareTo(Node other)
-            {
-                return weight - other.weight;
-            }
+public class Solution {
+    public int NetworkDelayTime(int[][] times, int n, int k) {
+      //build the graph
+      var graph = new Dictionary<int, List<(int, int)>>();
+      foreach(var time in times){
+        if(!graph.ContainsKey(time[0])){
+            graph[time[0]] = new();
         }
+        graph[time[0]].Add((time[1], time[2]));
+      }
+      
+      //build the distances map
+      var distances = new Dictionary<int, int>();
+      for(int i = 1; i <= n; i++){
+        distances[i] = int.MaxValue;
+      }
+      distances[k] = 0;
 
+      //build the heap
+      var heap = new PriorityQueue<(int node, int dist), int>();
+      heap.Enqueue((k, 0), 0);
 
-        public int NetworkDelayTime(int[][] times, int n, int k) {
-            Dictionary<int, List<Node>> graph = new();
-            Dictionary<int, int> map = new();
-            HashSet<int> seen = new();
-            var heap = new PriorityQueue<Node, int>();
+      //relaxation and update logic
+      while(heap.Count != 0){
+        var (src, currentDistance) = heap.Dequeue();
+        if(distances[src] < currentDistance) continue;
 
-            for(int i = 1; i <= n; i++){
-                graph.Add(i, new List<Node>());
-                map[i] = int.MaxValue; //current distance
-            }
-
-            foreach(var edge in times){
-                int src = edge[0], dst = edge[1], weight = edge[2];
-                graph[src].Add(new Node(dst, weight));
-            }
-
-            map[k] = 0; //starting node dist is 0
-            heap.Enqueue(new Node(k, 0), 0);
-            
-            //BFS
-            while(heap.Count != 0){
-                var node = heap.Dequeue();
-                
-                //check if its seen
-                if(seen.Contains(node.key)) continue;
-
-                //update the distance in map
-                map[node.key] = Math.Min(map[node.key], node.weight);
-
-                //check the neighbors/outgoing edges
-                foreach(var nei in graph[node.key]){
-                    var dist = map[node.key] + nei.weight; //update dist to time to reach parent + parent to nei
-                    map[nei.key] = Math.Min(map[nei.key], dist);
-                    heap.Enqueue(new Node(nei.key, map[nei.key]), map[nei.key]);
+        //iterate thru all neighbors
+        if(graph.ContainsKey(src)){
+            foreach(var (nei, dist) in graph[src]){
+                var newDistance = dist + currentDistance;
+                if(distances[nei] > newDistance){
+                    //relaxation
+                    distances[nei] = newDistance;
+                    heap.Enqueue((nei, newDistance), newDistance);
                 }
-
-                //add to seen
-                seen.Add(node.key);
             }
-            return map.Any(x=>x.Value == int.MaxValue) ? -1 : map.Max(x=>x.Value);
         }
+      }
+
+      if(distances.Any(x=>x.Value == int.MaxValue)) return -1; //unreachable node
+      return distances.Max(x=>x.Value);
     }
+}
