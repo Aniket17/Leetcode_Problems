42910aa8eea3631994e0dd52094819ce85fccbab
Aniket Dandawate <anidandawate1@gmail.com>
2024-11-02 14:56:17 +0530

    Time: 22 ms (55.12%), Space: 26.6 MB (63.72%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.cs
@@ -0,0 +1,18 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+public class Solution : VersionControl {
+    public int FirstBadVersion(int n) {
+        int lower = 1, higher = n;
+        while(lower < higher){
+            var mid = lower + (higher-lower)/2;
+            if(IsBadVersion(mid)){
+                //all right are bad
+                higher = mid;
+            }else{
+                lower = mid + 1;
+            }
+        }
+        return higher;
+    }
+}
