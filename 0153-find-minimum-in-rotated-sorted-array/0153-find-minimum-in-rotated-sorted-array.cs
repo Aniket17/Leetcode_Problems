7a13a5a1e5d479a24d03abbe8da16f2fb1e321cf
Aniket Dandawate <anidandawate1@gmail.com>
2024-11-02 15:09:28 +0530

    Time: 0 ms (100%), Space: 41 MB (47.06%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cs
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cs
@@ -3,13 +3,12 @@ public class Solution {
         var n = nums.Length;
         int left = 0, right = n - 1;
         while(left < right){
-            var mid = left + (right-left)/2;
-            if(nums[mid] < nums[right]){
-                right = mid;
-            }else if(nums[mid] > nums[right]){
-                left = mid+1;
+            var mid = left + (right - left)/2;
+            if(nums[mid] > nums[n-1]){
+                //min is in right
+                left = mid + 1;
             }else{
-                right--;
+                right = mid;
             }
         }
         return nums[left];
