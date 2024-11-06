d66953c24504f28f26c2e17dce0ceb1f4168e9ad
Aniket Dandawate <anidandawate1@gmail.com>
2024-11-06 17:38:35 +0530

    Time: 120 ms (24.22%), Space: 45.2 MB (36.58%) - LeetHub

--- 0322-coin-change/0322-coin-change.cs
+++ 0322-coin-change/0322-coin-change.cs
@@ -1,13 +1,16 @@
 public class Solution {
     int[] dp;
+    int minCoins = int.MaxValue;
     public int CoinChange(int[] coins, int amount) {
-        dp = new int[amount+1];
+        //amount < 0 return -1
+        //dp(amount) = 1 + dp(amount - coin);
+        dp = new int[amount + 1];
         Array.Fill(dp, -1);
-        var result = GetChange(coins, amount);
+        var result = CoinChangeUtil(coins, amount);
         return result == int.MaxValue ? -1 : result;
     }
 
-    int GetChange(int[] coins, int amount){
+    int CoinChangeUtil(int[] coins, int amount){
         if(amount < 0) return int.MaxValue;
         if(amount == 0){
             return 0;
@@ -16,7 +19,7 @@ public class Solution {
         var ans = int.MaxValue;
         foreach(var coin in coins){
             if(coin > amount) continue;
-            var res = GetChange(coins, amount - coin);
+            var res = CoinChangeUtil(coins, amount - coin);
             if(res != int.MaxValue){
                 res++;
                 ans = Math.Min(res, ans);
