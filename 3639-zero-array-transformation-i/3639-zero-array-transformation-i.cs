950aea47a95ff34996b9e4ca504927b56a945cf5
Aniket Dandawate <anidandawate1@gmail.com>
2024-11-30 12:40:49 +0530

    Time: 10 ms (30.56%), Space: 85.3 MB (98.15%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public bool IsZeroArray(int[] nums, int[][] queries) {
+        int n = nums.Length;
+        int[] diff = new int[n + 1];
+
+        // Apply the queries to the difference array
+        foreach (var query in queries) {
+            int li = query[0];
+            int ri = query[1];
+            diff[li]++;
+            if (ri + 1 < diff.Length) {
+                diff[ri + 1]--;
+            }
+        }
+
+        // Compute the prefix sum to get the actual effect of queries
+        for (int i = 1; i < n; i++) {
+            diff[i] += diff[i - 1];
+        }
+
+        // Check if nums can be transformed
+        for (int i = 0; i < n; i++) {
+            nums[i] -= diff[i];
+        }
+
+        // Ensure all elements are zero
+        return nums.All(x => x <= 0);
+    }
+}
