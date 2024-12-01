492055a922ee53fba1b90bd4d888f6d9040fc29b
Aniket Dandawate <anidandawate1@gmail.com>
2024-12-01 17:52:28 +0530

    Time: 34 ms (69.38%), Space: 68.6 MB (99.17%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cs
+++ 0721-accounts-merge/0721-accounts-merge.cs
@@ -3,25 +3,24 @@ public class Solution {
         var emailToParent = new Dictionary<string, string>();
         var emailToName = new Dictionary<string, string>();
 
-        // Union-Find Helper Functions
-        string Find(string email) {
-            if (emailToParent[email] != email) {
-                emailToParent[email] = Find(emailToParent[email]); // Path compression
+        string Find(string email){
+            if(emailToParent[email] != email){
+                emailToParent[email] = Find(emailToParent[email]);
             }
             return emailToParent[email];
         }
 
-        void Union(string email1, string email2) {
-            string parent1 = Find(email1);
-            string parent2 = Find(email2);
-            if (parent1 != parent2) {
-                emailToParent[parent2] = parent1;
+        void Union(string email1, string email2){
+            var p1 = Find(email1);
+            var p2 = Find(email2);
+            if(p1 != p2){
+                emailToParent[p1] = p2;
             }
         }
 
-        // Step 1: Initialize Union-Find and map emails to names
-        foreach (var account in accounts) {
-            string name = account[0];
+        for(int j = 0; j < accounts.Count; j++){
+            var account = accounts[j];
+            var name = account[0];
             for (int i = 1; i < account.Count; i++) {
                 if (!emailToParent.ContainsKey(account[i])) {
                     emailToParent[account[i]] = account[i];
@@ -33,26 +32,24 @@ public class Solution {
             }
         }
 
-        // Step 2: Group emails by their root parent
+        //merge by root
         var components = new Dictionary<string, List<string>>();
-        foreach (var email in emailToParent.Keys) {
-            string root = Find(email);
-            if (!components.ContainsKey(root)) {
-                components[root] = new List<string>();
+        foreach(var email in emailToParent.Keys){
+            var root = Find(email);
+            if(!components.ContainsKey(root)){
+               components[root] = new List<string>();
             }
             components[root].Add(email);
         }
-
-        // Step 3: Format the result
         var result = new List<IList<string>>();
-        foreach (var emails in components.Values) {
+        foreach(var component in components.Keys){
+            var name = emailToName[component];
+            var emails = components[component];
             emails.Sort(StringComparer.Ordinal);
-            string name = emailToName[emails[0]]; // All emails share the same name
-            var mergedAccount = new List<string> { name };
-            mergedAccount.AddRange(emails);
-            result.Add(mergedAccount);
+            var item = new List<string>(){name};
+            item.AddRange(emails);
+            result.Add(item);
         }
-
         return result;
     }
 }
