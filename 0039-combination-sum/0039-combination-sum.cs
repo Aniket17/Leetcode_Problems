6dd894b187859f55e7c015aa2fe9f2e253abd25e
Aniket Dandawate <anidandawate1@gmail.com>
2024-12-07 15:58:07 +0530

    Time: 158 ms (5.11%), Space: 62.5 MB (5.83%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cs
+++ 0039-combination-sum/0039-combination-sum.cs
@@ -1,19 +1,23 @@
 public class Solution {
     public IList<IList<int>> CombinationSum(int[] candidates, int target) {
         var ans = new List<IList<int>>();
-        Array.Sort(candidates);
-        Explore(candidates, target, ans, new List<int>());
+        var seen = new HashSet<string>();
+        Explore(candidates, target, ans, new List<int>(), seen);
         return ans;
     }
-    void Explore(int[] candidates, int target, List<IList<int>> ans, List<int> curr){
+    void Explore(int[] candidates, int target, List<IList<int>> ans, List<int> curr, HashSet<string> seen){
         if(target == 0){
+            var sorted = curr.OrderBy(x=>x);
+            var str = String.Join(",",sorted);
+            if(seen.Contains(str)) return;
+            seen.Add(str);
             ans.Add(curr.ToList());
             return;
         }
         for(int i = 0; i < candidates.Length; i++){
-            if(candidates[i] > target || curr.LastOrDefault() > candidates[i]) continue;
+            if(candidates[i] > target) continue;
             curr.Add(candidates[i]);
-            Explore(candidates, target - candidates[i], ans, curr);
+            Explore(candidates, target - candidates[i], ans, curr, seen);
             curr.RemoveAt(curr.Count - 1);
         }
     }
