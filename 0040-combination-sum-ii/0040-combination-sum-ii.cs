a4e0b3df0a1b92c8d653dbec47d37d09c657fd52
Aniket Dandawate <anidandawate1@gmail.com>
2024-12-07 16:25:06 +0530

    Time: 14 ms (14.86%), Space: 46.6 MB (55.45%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cs
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cs
@@ -2,10 +2,10 @@ public class Solution {
     public IList<IList<int>> CombinationSum2(int[] candidates, int target) {
         var ans = new List<IList<int>>();
         var counts = new Dictionary<int, int>();
+        Array.Sort(candidates);
         for(int i = 0; i < candidates.Length; i++){
             counts[candidates[i]] = counts.GetValueOrDefault(candidates[i]) + 1;
         }
-        counts = counts.OrderBy(x=>x.Key).ToDictionary(x=>x.Key, x=>x.Value);
         Solve(counts, target, new List<int>(), ans);
         return ans;
     }
