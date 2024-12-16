ff76cdfe48e337c326354d6dee93841a59ad65a2
Aniket Dandawate <anidandawate1@gmail.com>
2024-12-16 12:20:19 +0530

    Time: 0 ms (100%), Space: 49.2 MB (61.41%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cs
+++ 0022-generate-parentheses/0022-generate-parentheses.cs
@@ -1,34 +1,24 @@
 public class Solution {
-    List<string> answer = new();
-    public IList<string> GenerateParenthesis(int N) {
-        //number of open == number of closed
-        //open > closed, add closed
-        //open == closed add open
-        //open should always be >= closed
-        //start with open
-        //track number of open
-        //start with n = 2*n, number of open = 0
-        //reduce n and increase number of open
-        //backtrack
-        //when N == 0 exit
-        GenerateParenthesis(N, 0, 0, "");
-        return answer;
+    public IList<string> GenerateParenthesis(int n) {
+        var ans = new List<string>();
+        Generate(n, 0, 0, "", ans);
+        return ans;
     }
-    void GenerateParenthesis(int n, int open, int close, string res) {
-        if(res.Length == n * 2) {
-            answer.Add(res);
+
+    void Generate(int n, int open, int close, string curr, List<string> ans){
+        if(open == n && close == n){
+            ans.Add(curr);
             return;
         }
         if(open < n){
-            GenerateParenthesis(n, open+1, close, res+"(");
-            if(close < open){
-                GenerateParenthesis(n, open, close+1, res+")");
+            Generate(n, open + 1, close, curr+"(", ans);
+            if(open > close){
+                Generate(n, open, close + 1, curr + ")", ans);
             }
         }else if(open == close){
-            GenerateParenthesis(n, open+1, close, res+"(");
-        }
-        else if(close < n){
-            GenerateParenthesis(n, open, close+1, res+")");
+            Generate(n, open + 1, close, curr+"(", ans);
+        }else if(close < n){
+            Generate(n, open, close + 1, curr+")", ans);
         }
     }
 }
